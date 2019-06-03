f725f5270a76914e7a0160673ba99dd5bfc5bf28
Cheyin L <cheyinl@gmail.com>
2019-06-03 18:15:32 +0800

    support method lookup via reference

--- commonutil_net_thrift/client/roundrobin.py
+++ commonutil_net_thrift/client/roundrobin.py
@@ -4,22 +4,51 @@ Round-robin client
 """
 
 from contextlib import closing
+from typing import Any, Callable, Sequence, Tuple
 import logging
+
+from thrift.transport.TTransport import TTransportBase
+
+from commonutil_net_thrift.client.types import ClientSetup
 
 _log = logging.getLogger(__name__)
 
 
 class RoundRobinClient:
-	def __init__(self, connector_callable, client_cls, client_check_callable, server_locations, timeout_seconds=3, *args, **kwds):  # pylint: disable=too-many-arguments
+	"""
+	RoundRobinClient wraps given client class to provide auto re-connect in
+	round robin manner.
+	"""
+
+	__slots__ = (
+			"_client_setup",
+			"_connector_callable",
+			"_server_locations",
+			"_connect_kwds",
+			"_next_server_index",
+			"_client_callables",
+			"_client",
+			"_transport",
+	)
+
+	def __init__(
+			self,
+			client_setup: ClientSetup,
+			connector_callable: Callable[..., Tuple[Any, TTransportBase]],
+			server_locations: Sequence[Any],
+			timeout_seconds: float = 3,
+			*args,
+			**kwds,
+	):
 		super().__init__(*args, **kwds)
+		self._client_setup = client_setup
 		self._connector_callable = connector_callable
-		self._client_cls = client_cls
-		self._client_check_callable = client_check_callable
 		self._server_locations = server_locations
 		self._connect_kwds = {
 				"timeout_seconds": timeout_seconds,
 		}
 		self._next_server_index = 0
+		self._client_callables = ()
 		self._client = None
 		self._transport = None
 
@@ -39,26 +68,58 @@ class RoundRobinClient:
 			pass
 		self._client = None
 		self._transport = None
+		self._client_callables = ()
 
 	def _reconnect(self):
 		self.close()
-		srv_loc = self._server_locations[self._next_server_index]
-		self._next_server_index = (self._next_server_index + 1) % len(self._server_locations)
-		self._client, self._transport = self._connector_callable(self._client_cls, self._client_check_callable, *srv_loc, **self._connect_kwds)
+		remain_count = len(self._server_locations)
+		srv_loc = None
+		while remain_count > 0:
+			remain_count = remain_count - 1
+			try:
+				srv_loc = self._server_locations[self._next_server_index]
+				self._next_server_index = (self._next_server_index + 1) % len(self._server_locations)
+				self._client, self._transport = self._connector_callable(self._client_setup, *srv_loc, **self._connect_kwds)
+				return
+			except Exception as e:
+				_log.exception("failed on connecting to %r: %r", srv_loc, e)
 
-	def _invoke_impl(self, method_name, *args, **kwds):
+	def _invoke_with(self, invoke_impl_callable: Callable, method_ref: Any, *args, **kwds):
+		remain_count = len(self._server_locations)
+		while remain_count > 0:
+			remain_count = remain_count - 1
+			try:
+				return invoke_impl_callable(method_ref, *args, **kwds)
+			except Exception:
+				_log.exception("invoke (ref=%r) failed (remain-attempt=%r)", method_ref, remain_count)
+				self._reconnect()
+		return invoke_impl_callable(method_ref, *args, **kwds)
+
+	def _invoke_by_name_impl(self, method_name, *args, **kwds):
 		if not self._client:
 			self._reconnect()
 		client_callable = getattr(self._client, method_name)
 		return client_callable(*args, **kwds)
 
-	def _invoke(self, method_name, *args, **kwds):
-		remain_count = len(self.server_locations)
-		while remain_count > 0:
-			remain_count = remain_count - 1
-			try:
-				return self._invoke_impl(method_name, *args, **kwds)
-			except Exception:
-				_log.exception("invoke %r failed", method_name)
-				self._reconnect()
-		return self._invoke_impl(method_name, *args, **kwds)
+	def _invoke_by_name(self, method_name: str, *args, **kwds):
+		"""
+		(protected) _invoke_by_name call client method via lookuping up client
+		method callable with `getattr()`.
+		"""
+		return self._invoke_with(self._invoke_by_name_impl, method_name, *args, **kwds)
+
+	def _invoke_by_index_impl(self, callable_index, *args, **kwds):
+		if not self._client:
+			self._reconnect()
+		client_callable = self._client_callables[callable_index]
+		return client_callable(*args, **kwds)
+
+	def _invoke_by_index(self, callable_index: int, *args, **kwds):
+		"""
+		(protected) _invoke_by_index call client method via cached client
+		callables.
+
+		The client callables must cached into `_client_callables` property
+		with `ClientSetup.prepare_callable`.
+		"""
+		return self._invoke_with(self._invoke_by_index_impl, callable_index, *args, **kwds)
