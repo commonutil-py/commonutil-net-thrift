150685af7ee102a070188a7867b68fb318f0ea3e
Cheyin L <cheyinl@gmail.com>
2019-06-03 18:15:57 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup  # pylint: disable=import-error
+
+setup(
+		name="commonutil-net-thrift",
+		version="0.0.1",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		description="Helper routine for Thrift",
+		py_modules=[
+				"commonutil_net_thrift",
+				"commonutil_net_thrift.client",
+				"commonutil_net_thrift.connector",
+		],
+		classifiers=[
+				"Development Status :: 5 - Production/Stable",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 3.7",
+		],
+		license="MIT License",
+)
+
+# vim: ts=4 sw=4 ai nowrap
