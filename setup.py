263f0aaa9817238daa2378c6052bb4f05528d3c9
Cheyin L <cheyinl@gmail.com>
2019-06-04 02:18:00 +0800

    bump revision

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup  # pylint: disable=import-error
 
 setup(
 		name="commonutil-net-thrift",
-		version="3.0.1",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		version="3.0.2",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
 		description="Helper routine for Thrift",
 		packages=[
 				"commonutil_net_thrift",
